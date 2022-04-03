c58bd5e26e0fc21d4d7e3192f225934a818c98d5
Dinir Nertan <DinirNertan@gmail.com>
2022-04-03 22:46:28 +0900

    Add project files.

---
+++ Component.cs
@@ -0,0 +1,263 @@
+﻿using System;
+using System.IO;
+using UnityEngine;
+using UnityEngine.UI;
+using BallisticUnityTools.AssetApi;
+using BallisticUnityTools.Placeholders;
+using NgModding;
+using NgModding.Huds;
+using NgData;
+using NgSettings;
+using NgUi.MenuUi;
+using NgUi.RaceUi;
+using NgEvents;
+using NgPickups;
+using NgLib;
+using NgShips;
+using NgModes;
+using NgGame;
+using NgSp;
+using static Streamliner.HudRegister;
+
+namespace Streamliner
+{
+	public class Speedometer : ScriptableHud
+	{
+		private Image Panel;
+		private RectTransform PanelTransform;
+		private Text Value;
+		private Image GaugeBackground;
+		private RectTransform AccelGauge;
+		private float AccelMaxWidth;
+		private Vector2 _currentAccelSize;
+		private RectTransform SpeedGauge;
+		private float SpeedMaxWidth;
+		private Vector2 _currentSpeedSize;
+		private float _computedValue;
+
+		private float _currentSpeed;
+		private float _previousSpeed;
+		private Color _defaultColor;
+		private Color _highlightColor;
+		private float _colorTransitionTimer = 0.13f;
+
+		public override void Start()
+		{
+			base.Start();
+
+			Panel = CustomComponents.GetById<Image>("Panel");
+			PanelTransform = CustomComponents.GetById<RectTransform>("Panel");
+			Value = PanelTransform.GetChild(1).GetComponent<Text>();
+			GaugeBackground = CustomComponents.GetById<Image>("GaugeBackground");
+			AccelGauge = CustomComponents.GetById<RectTransform>("AccelGauge");
+			SpeedGauge = CustomComponents.GetById<RectTransform>("SpeedGauge");
+
+			// Gauges are stored in their maximum size, store the max width here.
+			AccelMaxWidth = AccelGauge.sizeDelta.x;
+			SpeedMaxWidth = SpeedGauge.sizeDelta.x;
+
+			// Initiate the gauges to have 0 width.
+			_currentAccelSize.x = 0;
+			_currentAccelSize.y = AccelGauge.sizeDelta.y;
+			_currentSpeedSize.x = 0;
+			_currentSpeedSize.y = SpeedGauge.sizeDelta.y;
+			AccelGauge.sizeDelta = _currentAccelSize;
+			SpeedGauge.sizeDelta = _currentSpeedSize;
+
+			// Colorizing
+			Value.color = GetTintColor();
+			GaugeBackground.color = GetTintColor(TextAlpha.ThreeEighths);
+			AccelGauge.GetComponent<Image>().color = GetTintColor(TextAlpha.Quarter);
+			SpeedGauge.GetComponent<Image>().color = GetTintColor();
+			_defaultColor = GetTintColor();
+			_highlightColor = new Color32(0x9f, 0x65, 0x67, 0xff);
+		}
+
+		public override void Update()
+		{
+			base.Update();
+
+			_currentAccelSize.x = GetHudAccelWidth();
+			_currentSpeedSize.x = GetHudSpeedWidth();
+			AccelGauge.sizeDelta = _currentAccelSize;
+			SpeedGauge.sizeDelta = _currentSpeedSize;
+			Value.text = GetSpeedValueString();
+
+			// Colorize the text on speed change
+			_currentSpeed = _currentSpeedSize.x;
+			ColorSpeedValue();
+			_previousSpeed = _currentSpeed;
+		}
+
+		private float GetHudAccelWidth() => 
+			Mathf.Clamp(TargetShip.PysSim.enginePower, 0f, 1f) * AccelMaxWidth;
+
+		private float GetHudSpeedWidth()
+		{
+			_computedValue = 
+				TargetShip.T.InverseTransformDirection(TargetShip.RBody.velocity).z /
+				TargetShip.Settings.ENGINE_MAXSPEED_SPECTRE;
+			_computedValue = Mathf.Clamp(
+				Mathf.Clamp(_computedValue * 3f, 0f, 1f), 0f, 1f);
+
+			return _computedValue * SpeedMaxWidth;
+		}
+
+		private string GetSpeedValueString() => 
+			IntStrDb.GetNumber(Mathf.Abs(Mathf.RoundToInt(TargetShip.HudSpeed)));
+
+		private void ColorSpeedValue()
+		{
+			if (!OptionSpeedHighlight)
+			{
+				return;
+			}
+			Color color = Value.color;
+			if (
+				_currentSpeed < _previousSpeed &&
+				TargetShip.BoostLevel == 0
+				)
+			{
+				// 4-frame transition in 60 fps => 60/4 = 15
+				color = Color.Lerp(color, _highlightColor, Time.deltaTime * 15f);
+			} 
+			else
+			{
+				color = Color.Lerp(color, _defaultColor, Time.deltaTime * 15f);
+			}
+			Value.color = color;
+		}
+	}
+
+	public class EnergyMeter : ScriptableHud
+	{
+		private Text Number;
+		private Image GaugeBackground;
+		private RectTransform Gauge;
+		private float MaxWidth;
+		private Vector2 _currentSize;
+		private float _computedValue;
+		private float _adjustedDamageMult;
+
+		public override void Start()
+		{
+			base.Start();
+
+			Number = CustomComponents.GetById<Text>("Number");
+			GaugeBackground = CustomComponents.GetById<Image>("GaugeBackground");
+			Gauge = CustomComponents.GetById<RectTransform>("Gauge");
+
+			// Gauge is stored in its maximum size, store the max width here.
+			MaxWidth = Gauge.sizeDelta.x;
+
+			// Initiate the gauge size.
+			_currentSize.x = MaxWidth;
+			_currentSize.y = Gauge.sizeDelta.y;
+			Gauge.sizeDelta = _currentSize;
+
+			// Coloring
+			Number.color = GetTintColor();
+			GaugeBackground.color = GetTintColor(TextAlpha.ThreeEighths);
+			Gauge.GetComponent<Image>().color = GetTintColor(TextAlpha.ThreeQuarters);
+		}
+
+		public override void Update()
+		{
+			base.Update();
+			
+			_currentSize.x = GetHudShieldWidth();
+			Gauge.sizeDelta = _currentSize;
+			Number.text = GetShieldValueString();
+
+		}
+
+		/*
+		 * # NgShips.ShipSettings
+		 * - DAMAGE_SHIELD = Shield Integrity
+		 * - DAMAGE_MULT = Shield Effectiveness
+		 * - DAMAGE_PWR = Weapon Effectiveness
+		 */
+
+		private float GetHudShieldWidth()
+		{
+			_computedValue =
+				TargetShip.ShieldIntegrity / TargetShip.Settings.DAMAGE_SHIELD;
+			_computedValue = Mathf.Clamp(_computedValue, 0f, 1f);
+
+			return _computedValue * MaxWidth;
+		}
+
+		private string GetShieldValueString()
+		{
+			if (Hud.ShieldDisplayMode == EShieldDisplayMode.Absolute)
+			{
+				_adjustedDamageMult = TargetShip.Settings.DAMAGE_MULT + 0.07f;
+				if (_adjustedDamageMult > 1f)
+				{
+					_adjustedDamageMult *= 1.1f;
+				}
+				else if (_adjustedDamageMult < 0f)
+				{
+					_adjustedDamageMult *= 0.9f;
+				}
+
+				_computedValue = Mathf.Ceil(
+					TargetShip.ShieldIntegrity / _adjustedDamageMult);
+				_computedValue = Mathf.Max(0f, _computedValue);
+			}
+			else
+			{
+				_computedValue = Mathf.Ceil(_computedValue * 100f) / 100f * 100f;
+			}
+
+			return TargetShip.Settings.DAMAGE_MULT <= 0f ? 
+				"" : 
+				IntStrDb.GetNoSingleCharNumber(Mathf.RoundToInt(_computedValue));
+		}
+	}
+
+	public class Timer : ScriptableHud
+	{}
+
+	public class SpeedLapTimer : ScriptableHud
+	{}
+
+	public class Placement : ScriptableHud
+	{}
+
+	public class LapCounter : ScriptableHud
+	{}
+
+	public class MessageLogger : ScriptableHud
+	{}
+
+	public class NowPlaying : ScriptableHud
+	{}
+
+	public class PickupDisplay : ScriptableHud
+	{}
+
+	public class ZoneTracker : ScriptableHud
+	{}
+
+	public class ZoneEnergyMeter : ScriptableHud
+	{}
+
+	public class TargetTimeDisplay : ScriptableHud
+	{}
+
+	public class TargetZoneDisplay : ScriptableHud
+	{}
+
+	public class Leaderboard : ScriptableHud
+	{}
+
+	public class CombatScoreboard : ScriptableHud
+	{}
+
+	public class TeamScoreboard : ScriptableHud
+	{}
+
+	public class UpsurgeScoreboard : ScriptableHud
+	{}
+}
---
+++ Initiate.cs
@@ -0,0 +1,279 @@
+﻿using System;
+using System.IO;
+using UnityEngine;
+using UnityEngine.UI;
+using BallisticUnityTools.AssetApi;
+using BallisticUnityTools.Placeholders;
+using NgModding;
+using NgModding.Huds;
+using NgData;
+using NgSettings;
+using NgUi.MenuUi;
+using NgUi.RaceUi;
+using NgEvents;
+using NgPickups;
+using NgLib;
+using NgShips;
+using NgModes;
+using NgGame;
+using NgSp;
+
+namespace Streamliner
+{
+	public class HudRegister : CodeMod
+	{
+		readonly string Id = "Streamliner";
+		public static ModAssets Assets;
+		private static string ModPathOnClassScope;
+
+		// options
+		public static Color[,] TintColorList = new Color[13, 5] {
+				{
+					new Color32(0xe4, 0xe4, 0xe4, 0xff),
+					new Color32(0xe4, 0xe4, 0xe4, 0xbf),
+					new Color32(0xe4, 0xe4, 0xe4, 0x7f),
+					new Color32(0xe4, 0xe4, 0xe4, 0x5f),
+					new Color32(0xe4, 0xe4, 0xe4, 0x3f)
+				},
+				{
+					new Color32(0xe4, 0xcf, 0xcf, 0xff),
+					new Color32(0xe4, 0xcf, 0xcf, 0xbf),
+					new Color32(0xe4, 0xcf, 0xcf, 0x7f),
+					new Color32(0xe4, 0xcf, 0xcf, 0x5f),
+					new Color32(0xe4, 0xcf, 0xcf, 0x3f)
+				},
+				{
+					new Color32(0xe4, 0xd9, 0xcf, 0xff),
+					new Color32(0xe4, 0xd9, 0xcf, 0xbf),
+					new Color32(0xe4, 0xd9, 0xcf, 0x7f),
+					new Color32(0xe4, 0xd9, 0xcf, 0x5f),
+					new Color32(0xe4, 0xd9, 0xcf, 0x3f)
+				},
+				{
+					new Color32(0xe4, 0xe3, 0xcf, 0xff),
+					new Color32(0xe4, 0xe3, 0xcf, 0xbf),
+					new Color32(0xe4, 0xe3, 0xcf, 0x7f),
+					new Color32(0xe4, 0xe3, 0xcf, 0x5f),
+					new Color32(0xe4, 0xe3, 0xcf, 0x3f)
+				},
+				{
+					new Color32(0xda, 0xe4, 0xcf, 0xff),
+					new Color32(0xda, 0xe4, 0xcf, 0xbf),
+					new Color32(0xda, 0xe4, 0xcf, 0x7f),
+					new Color32(0xda, 0xe4, 0xcf, 0x5f),
+					new Color32(0xda, 0xe4, 0xcf, 0x3f)
+				},
+				{
+					new Color32(0xcf, 0xe4, 0xcf, 0xff),
+					new Color32(0xcf, 0xe4, 0xcf, 0xbf),
+					new Color32(0xcf, 0xe4, 0xcf, 0x7f),
+					new Color32(0xcf, 0xe4, 0xcf, 0x5f),
+					new Color32(0xcf, 0xe4, 0xcf, 0x3f)
+				},
+				{
+					new Color32(0xcf, 0xe4, 0xd9, 0xff),
+					new Color32(0xcf, 0xe4, 0xd9, 0xbf),
+					new Color32(0xcf, 0xe4, 0xd9, 0x7f),
+					new Color32(0xcf, 0xe4, 0xd9, 0x5f),
+					new Color32(0xcf, 0xe4, 0xd9, 0x3f)
+				},
+				{
+					new Color32(0xcf, 0xe4, 0xe3, 0xff),
+					new Color32(0xcf, 0xe4, 0xe3, 0xbf),
+					new Color32(0xcf, 0xe4, 0xe3, 0x7f),
+					new Color32(0xcf, 0xe4, 0xe3, 0x5f),
+					new Color32(0xcf, 0xe4, 0xe3, 0x3f)
+				},
+				{
+					new Color32(0xcf, 0xda, 0xe4, 0xff),
+					new Color32(0xcf, 0xda, 0xe4, 0xbf),
+					new Color32(0xcf, 0xda, 0xe4, 0x7f),
+					new Color32(0xcf, 0xda, 0xe4, 0x5f),
+					new Color32(0xcf, 0xda, 0xe4, 0x3f)
+				},
+				{
+					new Color32(0xcf, 0xcf, 0xe4, 0xff),
+					new Color32(0xcf, 0xcf, 0xe4, 0xbf),
+					new Color32(0xcf, 0xcf, 0xe4, 0x7f),
+					new Color32(0xcf, 0xcf, 0xe4, 0x5f),
+					new Color32(0xcf, 0xcf, 0xe4, 0x3f)
+				},
+				{
+					new Color32(0xd9, 0xcf, 0xe4, 0xff),
+					new Color32(0xd9, 0xcf, 0xe4, 0xbf),
+					new Color32(0xd9, 0xcf, 0xe4, 0x7f),
+					new Color32(0xd9, 0xcf, 0xe4, 0x5f),
+					new Color32(0xd9, 0xcf, 0xe4, 0x3f)
+				},
+				{
+					new Color32(0xe3, 0xcf, 0xe4, 0xff),
+					new Color32(0xe3, 0xcf, 0xe4, 0xbf),
+					new Color32(0xe3, 0xcf, 0xe4, 0x7f),
+					new Color32(0xe3, 0xcf, 0xe4, 0x5f),
+					new Color32(0xe3, 0xcf, 0xe4, 0x3f)
+				},
+				{
+					new Color32(0xe4, 0xcf, 0xda, 0xff),
+					new Color32(0xe4, 0xcf, 0xda, 0xbf),
+					new Color32(0xe4, 0xcf, 0xda, 0x7f),
+					new Color32(0xe4, 0xcf, 0xda, 0x5f),
+					new Color32(0xe4, 0xcf, 0xda, 0x3f)
+				}
+			};
+		private string SettingsPath;
+		public static int OptionValueTint = 0;
+		public static bool OptionSpeedHighlight = true;
+
+		public enum TextAlpha
+		{
+			Full, ThreeQuarters, Half, ThreeEighths, Quarter
+		}
+
+		public static Color GetTintColor(TextAlpha transparencyIndex = TextAlpha.Full)
+		{
+			return TintColorList[OptionValueTint, (int)transparencyIndex];
+		}
+
+		public override void OnRegistered(string modPath)
+		{
+			ModPathOnClassScope = modPath;
+
+			// load from files
+			SettingsPath = Path.Combine(modPath, "settings.ini");
+
+			Assets = ModAssets.Load(
+				Path.Combine(modPath, "streamliner.nga"));
+
+			// register the mod
+			CustomHudRegistry.RegisterMod(Id);
+
+			RegisterManagers();
+
+			// options
+			ModOptions.OnLoadSettings += OnLoadSettings;
+			ModOptions.OnSaveSettings += OnSaveSettings;
+
+			ModOptions.RegisterMod("Streamliner", GenerateModUi, ModUiToCode);
+		}
+
+		private void RegisterManagers()
+		{
+			CustomHudRegistry.RegisterSceneManager(
+				"Race", Id, new RaceHudManager());
+			CustomHudRegistry.RegisterSceneManager(
+				"Speed Lap", Id, new SpeedLapHudManager());
+			CustomHudRegistry.RegisterSceneManager(
+				"Time Trial", Id, new TrialHudManager());
+			CustomHudRegistry.RegisterSceneManager(
+				"Eliminator", Id, new CombatHudManager());
+			CustomHudRegistry.RegisterSceneManager(
+				"Team Race", Id, new TeamRaceHudManager());
+			CustomHudRegistry.RegisterSceneManager(
+				"Survival", Id, new ZoneHudManager());
+			CustomHudRegistry.RegisterSceneManager(
+				"Upsurge", Id, new UpsurgeHudManager());
+			CustomHudRegistry.RegisterSceneManager(
+				"Knockout", Id, new KnockoutHudManager());
+			CustomHudRegistry.RegisterSceneManager(
+				"Rush Hour", Id, new RushHourHudManager());
+		}
+
+		private void GenerateModUi(ModOptionsUiContext ctx)
+		{
+			ctx.GenerateHeader("Display");
+
+			ctx.GenerateSelector(
+				"TextTint", "Text Tint",
+				"Change the colour of the texts.",
+				OptionValueTint,
+				"White", "Red", "Orange", "Yellow", "Lime", "Green", "Mint", "Cyan", "Azure", "Blue", "Violet", "Magenta", "Rose"
+			);
+
+			ctx.GenerateHeader("Additional Information");
+
+			ctx.GenerateSelector(
+				"SpeedHighlight", "Speed Reduction Highlight",
+				"Highlight speedometer text when speed decreases.",
+				OptionSpeedHighlight ? 1 : 0,
+				"Off", "On"
+			);
+		}
+
+		private void ModUiToCode(ModOptionsUiContext ctx)
+		{
+			OptionValueTint = ctx.GetSelectorValue("TextTint");
+			OptionSpeedHighlight = ctx.GetSelectorValue("SpeedHighlight") == 1;
+		}
+
+		private void OnLoadSettings()
+		{
+			INIParser ini = new INIParser();
+			ini.Open(SettingsPath);
+
+			OptionValueTint = ini.ReadValue("Display", "TextTint", OptionValueTint);
+			OptionSpeedHighlight = ini.ReadValue("AdditionalInformation", "SpeedHighlight", OptionSpeedHighlight);
+
+			ini.Close();
+		}
+
+		private void OnSaveSettings()
+		{
+			INIParser ini = new INIParser();
+			ini.Open(SettingsPath);
+
+			ini.WriteValue("Display", "TextTint", OptionValueTint);
+			ini.WriteValue("AdditionalInformation", "SpeedHighlight", OptionSpeedHighlight);
+
+			ini.Close();
+		}
+	}
+
+	public class RaceHudManager : SceneHudManager
+	{
+		public override void OnCreateHuds()
+		{
+			RegisterHud<Speedometer>(HudRegister.Assets.GetComponent<HudComponents>("Speed", false));
+			RegisterHud<EnergyMeter>(HudRegister.Assets.GetComponent<HudComponents>("Energy", false));
+		}
+	}
+
+	public class SpeedLapHudManager : SceneHudManager
+	{
+
+	}
+
+	public class TrialHudManager : SceneHudManager
+	{
+
+	}
+
+	public class CombatHudManager : SceneHudManager
+	{
+
+	}
+
+	public class TeamRaceHudManager : SceneHudManager
+	{
+
+	}
+
+	public class ZoneHudManager : SceneHudManager
+	{
+
+	}
+
+	public class UpsurgeHudManager : SceneHudManager
+	{
+
+	}
+
+	public class KnockoutHudManager : SceneHudManager
+	{
+
+	}
+
+	public class RushHourHudManager : SceneHudManager
+	{
+
+	}
+}
