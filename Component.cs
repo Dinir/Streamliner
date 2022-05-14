e0ce3896863d43424a595e3f351d968eaea79806
Dinir Nertan <DinirNertan@gmail.com>
2022-05-15 03:40:43 +0900

    change minimum position tracker distance to 48 sections

--- Component.cs
+++ Component.cs
@@ -1883,7 +1883,13 @@ namespace Streamliner
 	{
 		private const float AlphaEliminated = 0.5f;
 		private static int _totalSections;
-		private const int MinimumEndDistance = 16;
+		/* 
+		 * A ship 16 sections away is barely visible in horizontal splitscreen.
+		 * By multiplying this by third, half of one side of the tracker bar
+		 * can cover visible distance, and the outer half can cover
+		 * twice the distance beyond the visible one.
+		 */
+		private const int MinimumEndDistance = 16 * 3;
 		private static EPosHudMode _previousMode;
 		private bool _canShipRespawn;
 		private bool _initiated;
